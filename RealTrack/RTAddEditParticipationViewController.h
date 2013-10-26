2c166570ba31d9f1e40564d6be7aab5372d12b9a
Chenheli Hua <huachenheli@outlook.com>
2013-10-26 16:35:18 -0400

    Added add/edit participation view and its controller class.

---
+++ RealTrack/RTAddEditParticipationViewController.h
@@ -0,0 +1,16 @@
+//
+//  RTAddEditParticipationViewController.h
+//  RealTrack
+//
+//  Created by Chenheli Hua on 10/26/13.
+//  Copyright (c) 2013 Peace Corps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Projects.h"
+#import "Activities.h"
+#import "Participations.h"
+
+@interface RTAddEditParticipationViewController : UIViewController
+
+@end
---
+++ RealTrack/RTAddEditParticipationViewController.m
@@ -0,0 +1,38 @@
+//
+//  RTAddEditParticipationViewController.m
+//  RealTrack
+//
+//  Created by Chenheli Hua on 10/26/13.
+//  Copyright (c) 2013 Peace Corps. All rights reserved.
+//
+
+#import "RTAddEditParticipationViewController.h"
+
+@interface RTAddEditParticipationViewController ()
+
+@end
+
+@implementation RTAddEditParticipationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
