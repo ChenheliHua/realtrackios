12f8072e4b1cb258d2624900bc43eb04aa537fd4
Chenheli Hua <huachenheli@outlook.com>
2013-11-06 12:57:18 -0500

    Added delete calendar events when a project is deleted.

--- RealTrack/RTPendingViewController.h
+++ RealTrack/RTPendingViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Projects.h"
+#import "Activities.h"
 
 @interface RTPendingViewController : UITableViewController <NSFetchedResultsControllerDelegate>
 {
@@ -18,5 +20,9 @@
 // CoreData
 @property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
 @property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+// Projects and activities
+@property (strong, nonatomic) NSArray * projects;
+@property (strong, nonatomic) NSArray * activities;
 
 @end
